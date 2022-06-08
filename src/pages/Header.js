3ebb3e933e87763aca1179565fccc74e472b632e
Nasim Reza <nasim8382@gmail.com>
2022-06-08 23:43:57 +0600

    user information section modified

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 
 const Header = () => {
- const [user, setUser] = useState([]);
+  const [user, setUser] = useState([]);
 
   useEffect(() => {
     fetch("http://refertest.pythonanywhere.com/user/data", {
       method: "GET",
       Headers: {
-        "uid": 136
+        uid: 136,
       },
     })
       .then((res) => res.json())
@@ -15,18 +15,20 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <h2>Name: {user.name}</h2>
-        <h2>College: {user.college}</h2>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="dropdown dropdown-end">
-          <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img src={user.pictureUrl} alt="" />
-            </div>
-          </label>
+    <div className="navbar bg-base-100 pt-7">
+      <div className="mx-auto">
+        <div className="flex">
+          <h2 className="text-2xl mr-7">Name: <span className="font-bold">{user.name}</span></h2>
+          <h2 className="text-2xl mr-7">College: <span className="font-bold">{user.college}</span></h2>
+        </div>
+        <div>
+          <div className="dropdown dropdown-end">
+            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src={user.pictureUrl} alt="" />
+              </div>
+            </label>
+          </div>
         </div>
       </div>
     </div>
